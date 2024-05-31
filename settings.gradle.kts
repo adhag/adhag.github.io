c4a230af8f830957f1a9d76619d2b6647f645fd8
Adrian Häggvik <adha@netlight.com>
2024-05-31 13:32:31 +0200

    Gradle build scan fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,23 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.gradle.enterprise") version("3.17.4")
+    }
+}
+
+plugins {
+    id("com.gradle.enterprise") version "3.17.4"
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
 }
 
 dependencyResolutionManagement {
